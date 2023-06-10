c1bccb833f109af498d83ed90007fc2baf08f0e2
Ruan Reis <ruancosme23@gmail.com>
2023-06-10 01:47:55 -0300

    importando ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Index from './pages/Details/index.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Index from "./pages/Details/index.jsx";
+import { ThemeProvider } from "styled-components";
+import theme from './styles/theme.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Index />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <Index />
+    </ThemeProvider>
+  </React.StrictMode>
+);
