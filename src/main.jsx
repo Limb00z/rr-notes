d57c9f6309c80168d474fce36b35007d7a51dd4a
Ruan Reis <ruancosme23@gmail.com>
2023-06-13 14:21:38 -0300

    CRIANDO UM ESTILO DE TEMA GLOBAL

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Index from "./pages/Details/index.jsx";
 import { ThemeProvider } from "styled-components";
+import  GlobalStyle from './styles/global.js'
 import theme from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
       <Index />
     </ThemeProvider>
   </React.StrictMode>
