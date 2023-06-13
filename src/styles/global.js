d9619301eb2f2e9d385c612dd96f9361671ce986
Ruan Reis <ruancosme23@gmail.com>
2023-06-13 14:35:08 -0300

    Personalizando a font

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,10 +5,18 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        -webkit-font-smoothing: antialiased;
     }
     body{
         background: ${({theme}) => theme.COLORS.BACKGROUND_800};
         color: ${({theme}) => theme.COLORS.WHITE};
+    }
+
+    body, input, button, textarea{
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+        outline: none;
     }
 
     a{
