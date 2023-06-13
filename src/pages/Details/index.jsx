a6d97258087f177ba3699cbae1143bc33d060741
Ruan Reis <ruancosme23@gmail.com>
2023-06-13 19:48:35 -0300

    criando o Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,11 @@
 import { Container } from "./style"
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 
 function Index() {
    return (
     <Container>
-       
-       
+       <Header/>
        <Button title={"Voltar"}/>
     </Container>
   )
