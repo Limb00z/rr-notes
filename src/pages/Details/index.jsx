2be1e91e7e7f1c579fd8081f63f2c017fadc6712
Ruan Reis <ruancosme23@gmail.com>
2023-06-13 21:00:09 -0300

    Criado o component Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,22 +1,33 @@
-import { Container, Links } from "./style"
-import { Header } from "../../components/Header"
-import { Button } from "../../components/Button"
-import { Section } from "../../components/Section"
+import { Container, Links } from "./style";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 function Index() {
-   return (
+  return (
     <Container>
-         <Header />
-         <Section title="Links úteis" >
-            <Links>
-               <li><a href="#">link teste </a></li>
-               <li><a href="#">testando</a></li>
-               <li><a href="#">Coisa nova</a></li>
-            </Links>
-         </Section>
-       <Button title={"Voltar"}/>
+      <Header />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">link teste </a>
+          </li>
+          <li>
+            <a href="#">testando</a>
+          </li>
+          <li>
+            <a href="#">Coisa nova</a>
+          </li>
+        </Links>
+        </Section>
+        
+        <Section title="Marcadores">
+           
+        </Section>
+        
+      <Button title={"Voltar"} />
     </Container>
-  )
+  );
 }
 
-export default Index
+export default Index;
