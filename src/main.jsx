195f233d4efda4f2191888c2e03ef5dccbde2e97
Ruan Reis <ruancosme23@gmail.com>
2023-06-14 00:19:15 -0300

    Criação inicial da Page Home e separação dos GRIDS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Index from "./pages/Details/index.jsx";
+import {Home} from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import  GlobalStyle from './styles/global.js'
 import theme from './styles/theme.js'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Index />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );
