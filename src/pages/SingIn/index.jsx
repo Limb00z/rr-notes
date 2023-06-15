00f30ddfcf43e655b48e43ab3b3fbe5a0526ce16
Ruan Reis <ruancosme23@gmail.com>
2023-06-15 17:54:22 -0300

    Incrementando> Form, Input e Button

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,9 +1,31 @@
-import { Container } from './styles.js';
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Container, Form } from './styles.js';
+import { Button} from '../../components/Button'
+import { Input } from '../../components/Input'
+
 
 export function SingIn() {
     return (
         <Container>
-            
+            <Form>
+                <h1>RR-Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis</p>
+
+                <h2>Faça seu login</h2>
+                <Input
+                    placeholder="Email"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Entrar"/>
+            </Form>
        </Container> 
     )
 }
