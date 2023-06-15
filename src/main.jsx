9a9bd3c2d89656ac2ae6275714e35994ed5b17d6
Ruan Reis <ruancosme23@gmail.com>
2023-06-15 13:28:04 -0300

    Criando e testando a page de SingIn

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {Home} from "./pages/Home";
+import {SingIn} from "./pages/SingIn";
 import { ThemeProvider } from "styled-components";
 import  GlobalStyle from './styles/global.js'
 import theme from './styles/theme.js'
@@ -9,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Home />
+      <SingIn />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/SingIn/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles.js';
+
+export function SingIn() {
+    return (
+        <Container>
+            
+       </Container> 
+    )
+}
