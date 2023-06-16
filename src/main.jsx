3b75c87fcaeef9de6a423beae20d6df47402231e
Ruan Reis <ruancosme23@gmail.com>
2023-06-16 09:34:59 -0300

    Criando a page de  Profile

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import {SingIn} from "./pages/SingIn";
+import {Profile} from "./pages/Profile";
 import { ThemeProvider } from "styled-components";
 import  GlobalStyle from './styles/global.js'
 import theme from './styles/theme.js'
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <SingIn />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>
 );
