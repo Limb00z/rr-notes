01ca4ea7d058232ae1fd95d55ad7cf2da64bed5c
Ruan Reis <ruancosme23@gmail.com>
2023-06-16 09:42:53 -0300

    Criado os inputs do Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,7 @@
-import { Container } from "./styles";
-import { FiArrowLeft } from "react-icons/fi";
+import { Container, Form } from "./styles";
+import { FiArrowLeft,FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Profile() {
   return (
@@ -8,7 +10,32 @@ export function Profile() {
         <a href="#">
           <FiArrowLeft />
         </a>
-      </header>
+          </header>
+          
+          <Form>
+              <Input
+                  placeholder="Nome"
+                  type="text"
+                  icon={FiUser}
+              />
+              <Input
+                  placeholder="E-mail"
+                  type="text"
+                  icon={FiMail}
+              />
+              <Input
+                  placeholder="Senha atual"
+                  type="password"
+                  icon={FiLock}
+              />
+              <Input
+                  placeholder="Nova senha"
+                  type="password"
+                  icon={FiLock}
+              />
+
+              <Button title="Salvar"/>
+          </Form>
     </Container>
   );
 }
